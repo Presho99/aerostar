6a4909e0089f9ee36a8a97d117a264f4734a07ba
Precious <Preshomondi@gmail.com>
2024-03-09 05:41:37 +0300

    Edit map

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -1,22 +1,13 @@
-import React from 'react'
-import {MapContainer, TileLayer} from 'react-leaflet'
-import 'leaflet/dist/leaflet.css'
-
-// interface MapCenter {
-//     lat: number
-//     lng: number
-// }
+import React, { useEffect } from "react";
+import '../components/WorldMap.css'
+import {ReactComponent as World} from '../assets/map.svg'
 
 const WorldMap: React.FC = () => {
-    const defaultCenter: [number, number] = [0,0]
-  return (
-    <MapContainer   style={{height: '500px', width: '300px'}}>
-        <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        
-        />
-    </MapContainer>
-  )
-}
+ 
 
-export default WorldMap
+  return <div  className="world">
+<World style={{height: '150px'}}/>
+  </div>;
+};
+
+export default WorldMap;
